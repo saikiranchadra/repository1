0be1121e4e2bae462597280e508577acdfaebc93
Hemu2kvardhan <hemavardhan2k@gmail.com>
2023-04-08 17:15:12 +0530

    updated java file

--- sum.java
+++ sum.java
@@ -1,13 +1,13 @@
- package repository1;
-public class sum 
-{
-	public static void main(String[] kk)
-	{
-		int a,b,c;
-		a=10;
-		b=20;
-		c= a+b;
-		System.out.println("sum = "+c);
-		
-	}//close main
-}//close class
+package repository1;
+
+public class sum {
+	public static void main(String[] kk) {
+		int a, b, c;
+		a = 10;
+		b = 20;
+		c = a + b;
+		System.out.println("sum = " + c);
+		// viewed by hemavardhan
+
+	}// close main
+}// close class
