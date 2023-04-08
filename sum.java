a030a873de3b5544f24b6c4e071acfdb9d5b20ed
saikiranchadra <chandrasaikiran45@gmail.com>
2023-04-08 11:18:14 +0530

    added java file

---
+++ sum.java
@@ -0,0 +1,13 @@
+ package repository1;
+public class sum 
+{
+	public static void main(String[] kk)
+	{
+		int a,b,c;
+		a=10;
+		b=20;
+		c= a+b;
+		System.out.println("sum = "+c);
+		
+	}//close main
+}//close class
